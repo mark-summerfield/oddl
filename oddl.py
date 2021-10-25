e1bb7d41191e3c2a234a2e582d17a6fa7384e477
mark-summerfield <mark@qtrac.eu>
2021-10-25 20:09:23 +0100

    improved number regex

--- oddl.py
+++ oddl.py
@@ -513,14 +513,14 @@ NAME_RX = re.compile(r'[%$][A-Za-z_][0-9A-Za-z_]*')
 WS_RX = re.compile(r'[\s\n]+', re.DOTALL | re.MULTILINE)
 HEX_PATTERN = '[A-Fa-z\\d]'
 NUMBER_RX = re.compile( # does _not_ handle char-literal's
-    r'[-+]?(?:' # order: most restricted to least restricted
+    r'[-+]?(?:' # order: longest to shortest
+    r'(?:\d(?:_\d)*(?:\.\d(?:_\d)*)?|\.\d(?:_\d)*)' # float-literal
+    r'(?:(?:[eE][-+]?\d)?(?:_\d)*)?|' # optional exponent
+    r'\d(?:_?\d)*|' # decimal-literal
     r'0[bB][01](?:_[01])*|' # binary-literal
     r'0[oO][0-7](?:_[0-7])*|' # octal-literal
-    r'\d(?:_?\d)*|' # decimal-literal
-    r'0[xX][A-Fa-f\d](?:_[A-Fa-f\d])*|' # hex-literal / float-literal
-    r'(?:\d(?:_\d)*(?:\.\d(?:_\d)*)?|\.\d(?:_\d)*)(?:[eE][-+]?\d)?(?:_\d)*'
-    r')'
-    )
+    r'0[xX][A-Fa-f\d](?:_[A-Fa-f\d])*' # hex-literal
+    r')')
 
 CHAR_FOR_LITERAL = {"'": "'", '?': '?', 'a': '\a', 'b': '\b', 'f': '\f',
                     'n': '\n', 'r': '\r', 't': '\t', 'v': '\v'}
