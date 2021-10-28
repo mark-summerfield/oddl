db14b5fa53a360c8feb250fb018d77052902fecf
mark-summerfield <mark@qtrac.eu>
2021-10-28 10:01:56 +0100

    lint

--- st.sh
+++ st.sh
@@ -2,7 +2,7 @@
 tokei -slines -f -tPython -esetup.py -et.py
 unrecognized.py -q oddl.py
 python3 -m flake8 \
-    --ignore=E261,E303,W504 \
+    --ignore=E114,E116,E261,E303,W504 \
     oddl.py
 python3 -m vulture oddl.py
 ./t.py $@
